95c6c2defee080dcc28b2666ae7e45ef679c5ee1
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-02-24 10:15:53 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import VideoBk from './components/VideoBk'
 
 function App() {
+
+
+  const [userName, setName] = useState("shankur_11");
+  const [userImg, setImg] = useState("https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https://substack-post-media.s3.amazonaws.com/public/images/bec0d344-14a9-4a6a-9896-8d8b37508581_365x441.png");
+
   return (
-    <div>
-      <Header />
+    <div className='bg-neutral-900 w-screen h-screen overflow-hidden' >
+      <Header name={userName} img={userImg} />
       {/* <VideoBk /> */}
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Header() {
+function Header({ name, img }) {
+
+  const [ViewCount, setCount] = useState(100);
   return (
-    <div className='w-full h-[10vh]'>
-
+    <div className='w-full flex items-center justify-between mart-10 h-[10vh] text-white '>
+      <div className='flex gap-3 items-center h-full'>
+        <div className='marl-10 w-[35px] h-[35px] bg-gray-500 overflow-hidden flex items-center justify-center rounded-full'>
+          <img src={img} alt="" className='w-full h-full object-cover' />
+        </div>
+        <span className='text-xl'> {name}</span>
+      </div>
+      <div className='w-[30%] flex gap-4'>
+        <span className='bg-pink-600 button'>LIVE</span>
+        <span>{ViewCount}</span>
+      </div>
     </div>
   )
 }
