e7f9a5318933d45f45ec286eda6acdf5122dca38
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-02-24 10:37:12 +0530

    commit

--- src/components/VideoBk.jsx
+++ src/components/VideoBk.jsx
@@ -16,7 +16,7 @@ const VideoBk = () => {
 
   return (
     <div className="flex justify-center bg-green w-full items-center top-0 h-full fixed z-[1]">
-      <video ref={videoRef} autoPlay playsInline className="w-full h-full object-cover" />
+      <video ref={videoRef} autoPlay playsInline className="w-full h-full rotate-y-[180deg] object-cover" />
     </div>
   );
 };
