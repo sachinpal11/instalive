36ff4e3bc82b14e6e28333eedea2e79ea0b69a6f
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-02-24 10:21:52 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { IoIosArrowDown } from 'react-icons/io';
 
 function Header({ name, img }) {
 
   const [ViewCount, setCount] = useState(100);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomChange = Math.floor(Math.random() * 4) + 1;
+      const shouldIncrease = Math.random() > 0.5;
+
+      setCount(prev => prev + (shouldIncrease ? randomChange : -randomChange));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className='w-full flex items-center justify-between mart-10 h-[10vh] text-white '>
       <div className='flex gap-3 items-center h-full'>
         <div className='marl-10 w-[35px] h-[35px] bg-gray-500 overflow-hidden flex items-center justify-center rounded-full'>
           <img src={img} alt="" className='w-full h-full object-cover' />
         </div>
-        <span className='text-xl'> {name}</span>
+        <span className='text-xl flex items-center gap-1'> {name}<IoIosArrowDown /> </span>
       </div>
-      <div className='w-[30%] flex gap-4'>
+      <div className='w-[35%] justify-center flex gap-2'>
         <span className='bg-pink-600 button'>LIVE</span>
-        <span>{ViewCount}</span>
+        <span className='button bg-gray-600'>{ViewCount}</span>
       </div>
     </div>
   )
